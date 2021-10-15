5e65b79b45af03df6ae44acfee4dfee2b6a19042
Dawid.S <dawid.baltica@gmail.com>
2021-10-15 22:13:15 +0200

    Chapter added: Operatory logiczne

--- Podstawowe_struktury_danych.py
+++ Podstawowe_struktury_danych.py
@@ -29,3 +29,18 @@
 # print(x % y)   # 3      modulo - zwraca resztę z dzielenia
 # print(x ** y)  # 243    potęgowanie
 # print(-x)      # -3     liczba przeciwna
+
+
+# # Operatory logiczne ↓
+# x, y, z, numbers = 3, 5, 3, [1, 2, 3]
+# print(x == z)       # x jest równe od z
+# print(x != y)       # x jest różne od y
+# print(y > x)        # y jest większe od x
+# print(x >= z)       # x jest większe lub równe z
+# print(x < y)        # x jest mniejsze od y
+# print(x <= z)       # x jest mniejsze lub równe z
+# print(x and y)      # operator 'i'
+# print(x or y)       # operator 'lub'
+# print(x is x)       # operator 'jest'
+# print(x is not y)   # operator 'nie jest'
+# print(x in numbers) # operator 'zawiera się w..'
