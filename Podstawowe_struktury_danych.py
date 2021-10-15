4c47663d0c1df8609ff6557c4ad369ed8070b00f
Dawid.S <dawid.baltica@gmail.com>
2021-10-15 18:02:53 +0200

    Chapter added: Zmienne i typy

--- Podstawowe_struktury_danych.py
+++ Podstawowe_struktury_danych.py
@@ -3,3 +3,17 @@
 # oraz o dwóch spacji przed hashem i jedną za  # jeśli komentujesz inline
 # • Wcięcia - stosuj 4 spacje zamiast 1 tabulatora
 # • Maksymalna długość linii - ogranicz wszystkie wiersze do maksymalnie 79 znaków
+
+
+# # Zmienne i typy ↓
+# String = 'tekst'  # str(), Zmienna przechowująca napisy. Definiuj za pomocą pojedynczego lub podwójnego cudzysłowu
+# print(f'{String} {type(String)}')
+#
+# Integer = 5  # int(), Liczba całkowita
+# print(f'{Integer} {type(Integer)}')
+#
+# Float = 3.14  # float(), Liczba rzeczywista
+# print(f'{Float} {type(Float)}')
+#
+# Boolean = True  # bollean(), Typ logiczny. Przyjmuje wartości True(1) lub False(0)
+# print(f'{Boolean} {type(Boolean)}')
