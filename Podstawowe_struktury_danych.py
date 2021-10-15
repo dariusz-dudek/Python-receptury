f6f981a6b8be9ed2bc6668774ff1d2bc0947c4b6
Dawid.S <dawid.baltica@gmail.com>
2021-10-16 01:57:17 +0200

    fix syntax

--- Podstawowe_struktury_danych.py
+++ Podstawowe_struktury_danych.py
@@ -1,47 +1,37 @@
-# Zmienne dla poradnika
-text = 'Dowolny łańcuch znaków'
-one = 1
-three = 3
-five = 5
-pi = 3.14
-number_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-boolean = True
-
-
-# # Gramatyka pythona ↓    (PEP8: https://www.python.org/dev/peps/pep-0008/)
-# • Komentarze - używaj jednej spacji za hashem jeśli komentujesz pełną linię,
-#   oraz o dwóch spacji przed hashem i jedną za  # jeśli komentujesz inline
-# • Wcięcia - stosuj 4 spacje zamiast 1 tabulatora
-# • Maksymalna długość linii - ogranicz wszystkie wiersze do maksymalnie 79 znaków
-
+# Gramatyka pythona ↓    (PEP8: https://www.python.org/dev/peps/pep-0008/)
+# Komentarze - używaj jednej spacji za hashem jeśli komentujesz pełną linię,
+print('Hello, world!')  # oraz o dwóch spacji przed hashem i jedną za  # jeśli komentujesz inline
+# Wcięcia - stosuj 4 spacje zamiast 1 tabulatora
+# Maksymalna długość linii - ogranicz wszystkie wiersze do maksymalnie 79 znaków
 
 # Zmienne i typy ↓
-print(f'{text} {type(text)}')        # str(), łańcuchy znaków
-print(f'{one} {type(one)}')          # int(), liczba całkowita
-print(f'{pi} {type(pi)}')            # float(), liczba zmiennoprzecinkowa
-print(f'{boolean} {type(boolean)}')  # bollean(), typ logiczny. Przyjmuje wartości True(1) lub False(0)
-
+text = 'Dowolny łańcuch znaków'  # str(), łańcuchy znaków
+one = 1  # int(), liczba całkowita
+three = 3  # int(), liczba całkowita
+five = 5  # int(), liczba całkowita
+pi = 3.14  # float(), liczba zmiennoprzecinkowa
+boolean = True  # boll(), typ logiczny. Przyjmuje wartości True(1) lub False(0)
+number_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # Operatory arytmetyczne ↓
-print(three + five)   # 8    dodawanie
-print(five - three)   # 2    odejmowanie
-print(three * five)   # 15   mnożenie
-print(five / three)   # 0.6  dzielenie
-print(three // five)  # 0    dzielenie całkowite
-print(three % five)   # 3    modulo - zwraca resztę z dzielenia
-print(five ** three)  # 243  potęgowanie
-print(-one)           # -1   liczba przeciwna
-
+three + five  # dodawanie
+five - three  # odejmowanie
+three * five  # mnożenie
+five / three  # dzielenie
+three // five  # dzielenie całkowite
+three % five  # modulo - zwraca resztę z dzielenia
+five ** three  # potęgowanie
+-one  # liczba przeciwna
 
 # Operatory logiczne ↓
-print(one == 1)            # równe
-print(one != 10)           # różne
-print(five > 3)            # większe
-print(five >= 5)           # większe lub równe
-print(five < 10)           # mniejsze
-print(five <= 5)           # mniejsze lub równe
-print(one and five)        # 'i'
-print(one or three)        # 'lub'
-print(one is one)          # 'jest'
-print(one is not three)    # 'nie jest'
-print(one in number_list)  # 'zawiera się w..'
+one == 1  # równe
+one != 10  # różne
+five > 3  # większe
+five >= 5  # większe lub równe
+five < 10  # mniejsze
+five <= 5  # mniejsze lub równe
+one and five  # 'i'
+one or three  # 'lub'
+one is one  # 'jest'
+one is not three  # 'nie jest'
+one in number_list  # 'zawiera się w..'
