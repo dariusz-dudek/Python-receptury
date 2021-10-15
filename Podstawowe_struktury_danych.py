9a716b16c0add8c1f188e82b62fa82647a9ccd2a
Dawid.S <dawid.baltica@gmail.com>
2021-10-16 01:32:07 +0200

    fix syntax

--- Podstawowe_struktury_danych.py
+++ Podstawowe_struktury_danych.py
@@ -1,47 +1,47 @@
-x, y, z, numbers = 3, 5, 3, [1, 2, 3]
+# Zmienne dla poradnika
+text = 'Dowolny łańcuch znaków'
+one = 1
+three = 3
+five = 5
+pi = 3.14
+number_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+boolean = True
 
 
 # # Gramatyka pythona ↓    (PEP8: https://www.python.org/dev/peps/pep-0008/)
 # • Komentarze - używaj jednej spacji za hashem jeśli komentujesz pełną linię,
-# oraz o dwóch spacji przed hashem i jedną za  # jeśli komentujesz inline
+#   oraz o dwóch spacji przed hashem i jedną za  # jeśli komentujesz inline
 # • Wcięcia - stosuj 4 spacje zamiast 1 tabulatora
 # • Maksymalna długość linii - ogranicz wszystkie wiersze do maksymalnie 79 znaków
 
 
 # Zmienne i typy ↓
-String = 'tekst'  # str(), Zmienna przechowująca napisy. Definiuj za pomocą pojedynczego lub podwójnego cudzysłowu
-print(f'{String} {type(String)}')
-
-Integer = 5  # int(), Liczba całkowita
-print(f'{Integer} {type(Integer)}')
-
-Float = 3.14  # float(), Liczba rzeczywista
-print(f'{Float} {type(Float)}')
-
-Boolean = True  # bollean(), Typ logiczny. Przyjmuje wartości True(1) lub False(0)
-print(f'{Boolean} {type(Boolean)}')
+print(f'{text} {type(text)}')        # str(), łańcuchy znaków
+print(f'{one} {type(one)}')          # int(), liczba całkowita
+print(f'{pi} {type(pi)}')            # float(), liczba zmiennoprzecinkowa
+print(f'{boolean} {type(boolean)}')  # bollean(), typ logiczny. Przyjmuje wartości True(1) lub False(0)
 
 
 # Operatory arytmetyczne ↓
-print(x + y)   # 8      dodawanie
-print(x - y)   # 2      odejmowanie
-print(x * y)   # 15     mnożenie
-print(x / y)   # 0.6    dzielenie
-print(x // y)  # 0      dzielenie całkowite
-print(x % y)   # 3      modulo - zwraca resztę z dzielenia
-print(x ** y)  # 243    potęgowanie
-print(-x)      # -3     liczba przeciwna
+print(three + five)   # 8    dodawanie
+print(five - three)   # 2    odejmowanie
+print(three * five)   # 15   mnożenie
+print(five / three)   # 0.6  dzielenie
+print(three // five)  # 0    dzielenie całkowite
+print(three % five)   # 3    modulo - zwraca resztę z dzielenia
+print(five ** three)  # 243  potęgowanie
+print(-one)           # -1   liczba przeciwna
 
 
 # Operatory logiczne ↓
-print(x == z)       # x jest równe od z
-print(x != y)       # x jest różne od y
-print(y > x)        # y jest większe od x
-print(x >= z)       # x jest większe lub równe z
-print(x < y)        # x jest mniejsze od y
-print(x <= z)       # x jest mniejsze lub równe z
-print(x and y)      # operator 'i'
-print(x or y)       # operator 'lub'
-print(x is x)       # operator 'jest'
-print(x is not y)   # operator 'nie jest'
-print(x in numbers) # operator 'zawiera się w..'
+print(one == 1)            # równe
+print(one != 10)           # różne
+print(five > 3)            # większe
+print(five >= 5)           # większe lub równe
+print(five < 10)           # mniejsze
+print(five <= 5)           # mniejsze lub równe
+print(one and five)        # 'i'
+print(one or three)        # 'lub'
+print(one is one)          # 'jest'
+print(one is not three)    # 'nie jest'
+print(one in number_list)  # 'zawiera się w..'
