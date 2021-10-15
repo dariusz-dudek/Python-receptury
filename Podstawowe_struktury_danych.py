46b2a2caec48b3611b5c0a696a317917c2ff0046
Dawid.S <dawid.baltica@gmail.com>
2021-10-15 18:01:28 +0200

    Chapter added: Gramatyka pythona

---
+++ Podstawowe_struktury_danych.py
@@ -0,0 +1,5 @@
+# # Gramatyka pythona ↓    (PEP8: https://www.python.org/dev/peps/pep-0008/)
+# • Komentarze - używaj jednej spacji za hashem jeśli komentujesz pełną linię,
+# oraz o dwóch spacji przed hashem i jedną za  # jeśli komentujesz inline
+# • Wcięcia - stosuj 4 spacje zamiast 1 tabulatora
+# • Maksymalna długość linii - ogranicz wszystkie wiersze do maksymalnie 79 znaków
