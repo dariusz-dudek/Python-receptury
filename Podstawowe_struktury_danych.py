14ea9175e61b08ee692db863f69d3d6d35af7388
Dawid.S <dawid.baltica@gmail.com>
2021-10-16 02:23:30 +0200

    fix syntax

--- Podstawowe_struktury_danych.py
+++ Podstawowe_struktury_danych.py
@@ -1,37 +1,38 @@
 # Gramatyka pythona ↓    (PEP8: https://www.python.org/dev/peps/pep-0008/)
 # Komentarze - używaj jednej spacji za hashem jeśli komentujesz pełną linię,
-print('Hello, world!')  # oraz o dwóch spacji przed hashem i jedną za  # jeśli komentujesz inline
+print('Hello, world!')  # oraz o dwóch spacji przed hashem i jedną za hashem jeśli komentujesz inline
 # Wcięcia - stosuj 4 spacje zamiast 1 tabulatora
 # Maksymalna długość linii - ogranicz wszystkie wiersze do maksymalnie 79 znaków
 
 # Zmienne i typy ↓
 text = 'Dowolny łańcuch znaków'  # str(), łańcuchy znaków
-one = 1  # int(), liczba całkowita
-three = 3  # int(), liczba całkowita
-five = 5  # int(), liczba całkowita
+number = 1  # int(), liczba całkowita
 pi = 3.14  # float(), liczba zmiennoprzecinkowa
-boolean = True  # boll(), typ logiczny. Przyjmuje wartości True(1) lub False(0)
-number_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+its_true = True  # boll(), typ logiczny. Przyjmuje wartości True(1) lub False(0)
+number_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # lista
+animal_tuple = ('dog', 'cat', 'mouse')  # tuple/krotka
 
 # Operatory arytmetyczne ↓
-three + five  # dodawanie
-five - three  # odejmowanie
-three * five  # mnożenie
-five / three  # dzielenie
-three // five  # dzielenie całkowite
-three % five  # modulo - zwraca resztę z dzielenia
-five ** three  # potęgowanie
--one  # liczba przeciwna
+3 + 5  # dodawanie
+5 - 3  # odejmowanie
+3 * 5  # mnożenie
+10 / 2  # dzielenie
+5 // 2  # dzielenie całkowite
+5 % 3  # modulo - zwraca resztę z dzielenia
+3 ** 2  # potęgowanie
+-1  # liczba przeciwna
 
 # Operatory logiczne ↓
-one == 1  # równe
-one != 10  # różne
-five > 3  # większe
-five >= 5  # większe lub równe
-five < 10  # mniejsze
-five <= 5  # mniejsze lub równe
-one and five  # 'i'
-one or three  # 'lub'
-one is one  # 'jest'
-one is not three  # 'nie jest'
-one in number_list  # 'zawiera się w..'
+1 == 1  # równe
+1 != 7  # różne
+5 > 3  # większe
+5 >= 5  # większe lub równe
+5 < 7  # mniejsze
+5 <= 5  # mniejsze lub równe
+'cat' and 'mouse'  # 'i'
+'dog' or 'cat'  # 'lub'
+True is its_true  # 'jest'
+animal_tuple is not number_list  # 'nie jest'
+'cat' in animal_tuple  # 'zawiera się w..'
+
+
